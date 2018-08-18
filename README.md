33c6c745b4a995ebb6fedf364642e2f4b67cd249
Hiroshi Takekawa <sian@big.or.jp>
2018-08-18 11:07:35 +0900

    iter.jl: Update to adapt for Julia 1.0

--- README.md
+++ README.md
@@ -28,7 +28,7 @@ Introduction
 - [Julia](https://julialang.org/) is a high-level, high-performance dynamic programming language for numerical computing (quoted from the site).
 - This directory contains some examples of the Julia scripts to get a picture of what the Julia language is.
 - Note that these scripts are for demonstation and understanding the Julia language.  I cannot ensure any mathematical correctness or efficiency.
-- These scripts are confirmed on Julia: Version 0.7.0-DEV.4456 Commit 05ff71f187 (2018-03-03 01:29 UTC)
+- These scripts are confirmed on Julia: Version 1.1.0-DEV.42 Commit cd5e5a38c5* (2018-08-17 05:55 UTC)
 
 
 Sample
--- iter/iter.jl
+++ iter/iter.jl
@@ -3,38 +3,45 @@
 #
 # Iterator sample
 # Copyright(C) Hiroshi Takekawa
+#
+# Based on the sample code snippets from https://docs.julialang.org/en/latest/manual/interfaces/
 
-# See https://docs.julialang.org/en/latest/manual/interfaces/
-# Almost all code are shamelessly stolen from this site.
+using Statistics
+
 struct Squares
 	count::Int
 end
 
 # Squares implementation
-Base.start(::Squares) = 1
-Base.next(S::Squares, state) = (state*state, state+1)
-Base.done(S::Squares, state) = state > S.count
+Base.firstindex(::Squares) = 1
+Base.lastindex(S::Squares) = length(S)
+Base.iterate(S::Squares, state = 1) = state > S.count ? nothing : (state * state, state + 1)
 Base.eltype(::Type{Squares}) = Int # Note that this is defined for the type
 Base.length(S::Squares) = S.count
 
+println("Squares from 1 to 7")
 for i in Squares(7)
 	println(i)
 end
+println("")
 
 if 25 in Squares(6)
-	println("25 is in Squares(6)")
+	println("25 is in the set of Squares(6)")
 end
+println("")
 
-println("Means: 1^2 + ... + 100+2");
-println(mean(Squares(100)))
+println("Means: 1^2 + .. + 100^2");
+println(Statistics.mean(Squares(100)))
+println("")
 
-Base.sum(S::Squares) = (n = S.count; return n*(n+1)*(2n+1)÷6)
-println("1^2 + .. + 1803^3")
+Base.sum(S::Squares) = (n = S.count; return (n * (n + 1) * (2n + 1) ÷ 6))
+println("1^2 + .. + 1803^2")
 println(sum(Squares(1803)))
+println("")
 
 function Base.getindex(S::Squares, i::Int)
 	1 <= i <= S.count || throw(BoundsError(S, i))
-	return i*i
+	return i * i
 end
-println("Squares(100)[23]")
+println("Squares(100)[23], which creates the set of squares from 1 to 100, then return 23th element")
 println(Squares(100)[23]);
