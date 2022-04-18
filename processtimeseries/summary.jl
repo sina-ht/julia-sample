c12e6461237ecf5a6be00c1de11bf53592909fdc
Hiroshi Takekawa <sian.ht@gmail.com>
2022-04-18 23:25:04 +0900

    processtimeseries: Use collapse() to calculate a mean per month instead of moving avg.
    Signed-off-by: Hiroshi Takekawa <sian.ht@gmail.com>

--- processtimeseries/summary.jl
+++ processtimeseries/summary.jl
@@ -9,22 +9,31 @@ using CSV, DataFrames, Dates, TimeSeries, Statistics, Plots; pyplot()
 rawdata = CSV.read("data.csv", DataFrame; types = [String, Int, Int, Int, Float64, Float64, String], missingstring = "NA")
 # Drop N/A.
 data = dropmissing(rawdata, :weight)
+data = dropmissing(data, :low)
 
 ### Convert data for further processing
 # Parse date
 format = Dates.DateFormat("m/d/y H:M:S")
-xdates = DateTime.(data[:, :date], format)
+xdates = Date.(data[:, :date], format)
 # Insert dates as timestamps
 data[:, :timestamp] = xdates
 # Convert DataFrame into TimeArray
 ta = TimeArray(data, timestamp = :timestamp)
 
+# Calculate means per month
+monthlyw = collapse(ta[:weight], month, last, mean)
+monthlyl = collapse(ta[:low], month, last, mean)
+
 ### Plot
-# Plot moving average of weight
-plot(moving(mean, ta[:weight], 90), yticks = 60:5:100, title = "Weight/Low", label = "Weight", xlabel = "Date", ylabel = "Value", legend = :bottomright, color=:red)
-plot!(moving(mean, ta[:low], 90), label = "Low", xlabel = "Date", color=:black)
+plot(monthlyw, yticks = 60:5:100, title = "Weight/Low", label = "Weight", xlabel = "Date", ylabel = "Value", legend = :bottomright, color=:red)
+plot!(monthlyl, label = "Low", xlabel = "Date", color=:black)
 
 # Resize
 plot!(size=(1024, 768))
 
 savefig("output.png")
+
+# Is there any correlation between weight and low blood pressure?
+println("Correation coefficient form the beginning to years")
+cors = [cor(values(to(ta, Date(Year(y)))[:weight]), values(to(ta, Date(Year(y)))[:low])) for y in 2023:2032]
+println(cors)
